7adf8a5a2598346290e2efa13689b5e2a66657c6
DEEPTI BS <deeptibalgar@gmail.com>
2026-02-24 20:50:54 +0530

    Correct band name generator function and UI
    Fix function name and improve GUI elements

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import tkinter as tk
 
+def generate_band-name():
+
+  city = city_entry.get()
+
+  pet = pet_entry.get()
+
+  result = "your band name could be "+ city + " " + pet
+
+  result_label.config(text = result)
+
+root = tk.tk()
+
+root.title("BAND NAME GENERATOR")
+
+root.geometry("400*300")
+
+tk.Label(root,text = "city you grew up in:").pack()
+
+city_entry.pack()
+
+tk.Label(root,text = "pet.name:").pack()
+
+pet_entry = tk.Entry
